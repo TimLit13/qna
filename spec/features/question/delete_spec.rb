066b528c6ab1e2cae77742be072a3e6afd2413c5
“TimLit13” <litidm@gmail.com>
2022-05-24 08:28:43 +0300

    Add feature tests for delete questions

---
+++ spec/features/question/delete_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+feature 'User can delete quetion' do
+  given(:user) { create(:user) }
+  given(:question_author) { create(:user) }
+  given(:question) { create(:question, user: question_author) }
+
+  describe 'Authenticated user' do
+    scenario 'Author can delete his question' do
+      sign_in(question_author)
+      visit question_path(question)
+      click_on 'Delete'
+
+      expect(page).to have_content 'Question was successfully deleted'
+      expect(page).to have_not_content question.body
+    end
+    scenario 'Other user can not delete author question' do
+      sign_in(user)
+      visit question_path(question)
+
+      expect(page).to_not have_selector(:link_or_button, 'Delete')
+    end
+  end
+
+  describe 'Unuthenticated user' do
+    scenario 'User can not delete question' do
+      visit question_path(question)
+      expect(page).to_not have_selector(:link_or_button, 'Delete')
+    end 
+  end
+
+end
