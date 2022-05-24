113571fe979fc630ce8c3df4fe3cd295c602f484
“TimLit13” <litidm@gmail.com>
2022-05-24 07:29:14 +0300

    Refactor extract answers test into feature answer

--- spec/features/question/show_spec.rb
+++ spec/features/question/show_spec.rb
@@ -1,9 +1,6 @@
 require 'rails_helper'
 
-feature 'User can see question and answers and can create answer', '
-  In order to answer the question
-  User would like to be able to create the answer
-' do
+feature 'User can see question and answers' do
   given(:user) { create(:user) }
   given(:question) { create(:question, user: user) }
   given!(:answers) { create_list(:answer, 3, question: question, user: user) }
@@ -12,22 +9,5 @@ feature 'User can see question and answers and can create answer', '
 
   scenario 'Can see list of answers' do
     expect(page).to have_content('MyAnswerText', count: 3)
-  end
-
-  scenario 'Can see form for create new answer' do
-    expect(page).to have_selector(:link_or_button, 'Create answer')
-  end
-
-  scenario 'Can create an answer' do
-    fill_in 'Body', with: 'Answer text'
-    click_on 'Create answer'
-
-    expect(page).to have_content 'Your answer successfully created!'
-    expect(page).to have_content 'Answer text'
-  end
-
-  scenario 'Create an answer with errors' do
-    click_on 'Create answer'
-    expect(page).to have_content "can't be blank"
   end
 end
