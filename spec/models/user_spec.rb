5fe2c7758288ad42fedce4ca8d65603acd2e75d5
“TimLit13” <litidm@gmail.com>
2022-05-25 22:26:14 +0300

    Refactor extract method author_of? for user model and add tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,20 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :email }
     it { should validate_presence_of :password }
   end
+
+  describe '.author_of?' do
+    context 'user is author of any resource' do
+      let(:first_user) { create(:user) }
+      let(:question) { create(:question, user: first_user) }
+      let(:second_user) { create(:user) }
+
+      it 'user is an author of resource' do
+        expect(first_user.author_of?(question)).to be_truthy
+      end
+
+      it 'user is not an author of resource' do
+        expect(second_user.author_of?(question)).to be_falsy
+      end
+    end
+  end
 end
