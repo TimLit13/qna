7f67519896bcefcdb489cfae780c5e9e358df316
tim <litidm@gmail.com>
2022-08-02 21:45:06 +0300

    Create api response for api/v1/profiles/all

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -2,4 +2,10 @@ class Api::V1::ProfilesController < Api::V1::BaseController
   def me
     render json: current_resource_owner
   end
+
+  def all
+    user_profiles = User.where.not(id: current_resource_owner.id)
+
+    render json: user_profiles # , serializer: UserSerializer
+  end
 end
--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -40,13 +40,65 @@ RSpec.describe 'Profiles API', type: :request do
 
       it 'returns all public fields' do
         %w[id email admin created_at updated_at].each do |attr|
-          expect(json[attr]).to eq(user.send(attr).as_json)
+          expect(json['user'][attr]).to eq(user.send(attr).as_json)
         end
       end
 
       it 'does not return private fields' do
         %w[password encrypted_password].each do |attr|
           expect(json).to_not have_key(attr)
+        end
+      end
+    end
+  end
+
+  describe 'GET /api/v1/profiles/all' do
+    it_behaves_like 'API Authorizable' do
+      let(:method) { :get }
+      let(:api_path) { '/api/v1/profiles/all' }
+    end
+
+    context 'unauthorized' do
+      it 'returns 401 status if the is no access_token' do
+        get '/api/v1/profiles/all', headers: headers
+        # expect(response.status).to eq(401)
+        expect(response).to have_http_status(:unauthorized)
+      end
+
+      it 'returns 401 if access_token is invalid' do
+        get '/api/v1/profiles/all', params: { access_token: 'smth' }, headers: headers
+        
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+
+    context 'authorized' do
+      let!(:user) { create(:user) }
+      let!(:other_user) { create(:user) }
+      let(:access_token) { create(:access_token, resource_owner_id: user.id) }
+
+      before do
+        get '/api/v1/profiles/all', params: { access_token: access_token.token }, headers: headers
+      end
+
+      it 'returns 20x status' do
+        expect(response).to be_successful
+      end
+
+      it 'returns users list without one' do
+        expect(json['users'].count).to eq(User.count - 1)
+        expect(json['users'].count).to eq(1)
+      end
+
+      it 'returns all public fields' do
+        %w[id email created_at updated_at].each do |attr|
+          expect(json['users'].first[attr]).to eq(other_user.send(attr).as_json)
+        end
+      end
+
+      it 'does not return private fields' do
+        %w[password encrypted_password].each do |attr|
+          expect(json['users'].first).to_not have_key(attr)
         end
       end
     end
