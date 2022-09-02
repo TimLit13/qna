f04e76d6b75b91426f4fdc0fb4de6029e3d13e86
tim <litidm@gmail.com>
2022-09-02 05:49:14 +0300

    Fix for action mailer settings

---
+++ config/deploy.rb
@@ -0,0 +1,21 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.17.1"
+
+set :application, "qna"
+set :repo_url, "git@github.com:TimLit13/qna.git"
+
+# Default branch is :master, need main
+# ask :main, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/home/deployer/qna"
+set :deploy_user, 'deployer'
+set :branch, "main"
+
+set :pty, false
+
+# Default value for :linked_files is []
+append :linked_files, "config/database.yml", 'config/master.key'
+
+# Default value for linked_dirs is []
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "storage"
