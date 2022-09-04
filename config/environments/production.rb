dfbdb6d6ac706d1fad7acc70857d745d88bd18fb
tim <litidm@gmail.com>
2022-09-04 11:13:59 +0300

    hardcode username, password for gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,8 +68,10 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    user_name: ENV['SMTP_USERNAME'],
-    password: ENV['SMTP_PASSWORD'],
+    # user_name: ENV['SMTP_USERNAME'],
+    # password: ENV['SMTP_PASSWORD'],
+    user_name: 'litidm@gmail.com',
+    password: 'qodddzasaqueqbwy',
     authentication: 'plain',
     enable_starttls_auto: true
   }
